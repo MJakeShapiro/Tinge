c71ddaee14deb651ac376f4b6e7632e0a3ec912e
rhwjwkdeks <rhwjwkdeks@gmail.com>
2020-10-19 17:53:14 -0500

    test

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    Rigidbody2D myRigidBody;
+    BoxCollider2D boxCollider;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Jump();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(runSpeed * controlThrow, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+
+    void Jump()
+    {
+        if (!boxCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+
+}
