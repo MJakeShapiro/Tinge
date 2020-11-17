5734fd697caf255f5c4a5d5f9bc712be8e1aeb6b
rhwjwkdeks <rhwjwkdeks@gmail.com>
2020-11-17 15:24:31 -0600

    Falling rubble script

---
+++ Assets/tornado/Rubble.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rubble : MonoBehaviour
+{
+    private Vector2 MoveDiriection;
+    private float moveSpeed;
+    private CircleCollider2D circleCollider;
+
+    private void OnEnable()
+    {
+        StartCoroutine(trig());
+        Invoke("Destroy", 1.5f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+        moveSpeed = 5f;
+        circleCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(MoveDiriection * moveSpeed * Time.deltaTime);
+    }
+
+    IEnumerator trig()
+    {
+        yield return new WaitForSeconds(1f);
+        circleCollider.isTrigger = true;
+    }
+
+    public void SetMoveDirection(Vector2 dir)
+    {
+        MoveDiriection = dir;
+    }
+
+    public void Destroy()
+    {
+        gameObject.SetActive(false);
+        circleCollider.isTrigger = false;
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
---
+++ Assets/tornado/RubblePool.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubblePool : MonoBehaviour
+{
+
+    public static RubblePool rubblePoolInstance;
+
+    [SerializeField]
+    private GameObject pooledRubble;
+    private bool notEnoughRubblesInPool = true;
+
+    private List<GameObject> rubbles;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        rubblePoolInstance = this;
+    }
+
+    void Start()
+    {
+        rubbles = new List<GameObject>();
+    }
+
+    public GameObject GetRubble()
+    {
+        if(rubbles.Count > 0)
+        {
+            for (int i = 0; i<rubbles.Count; i++)
+            {
+                if (!rubbles[i].activeInHierarchy)
+                {
+                    return rubbles[i];
+                }
+            }
+        }
+
+        if (notEnoughRubblesInPool)
+        {
+            GameObject rub = Instantiate(pooledRubble);
+            rub.SetActive(false);
+            rubbles.Add(rub);
+            return rub;
+        }
+
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
