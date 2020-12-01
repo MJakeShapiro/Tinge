328e0e669250c5f2eadde899987a72f1519baf16
Drew Williams <61474426+DDub97@users.noreply.github.com>
2020-11-30 20:26:20 -0600

    Main Menu & Quit on ESC Key

---
+++ Assets/Scripts/ESC_Quit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESC_Quit : MonoBehaviour
+{
+    public QuitGame quit;
+    void Update()
+    {
+        if(Input.GetKeyDown("escape")){
+            quit.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+    #if UNITY_STANDALONE
+        Application.Quit();
+    #endif
+ 
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+    }
+}
