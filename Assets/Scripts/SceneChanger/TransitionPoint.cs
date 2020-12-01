eaac837946045eb3f5b4f32079de971aa4bfd804
MJakeShapiro <49694168+MJakeShapiro@users.noreply.github.com>
2020-12-01 16:49:05 -0600

    Transition fix for build

--- Assets/Scripts/SceneChanger/TransitionPoint.cs
+++ Assets/Scripts/SceneChanger/TransitionPoint.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class TransitionPoint : MonoBehaviour
 {
 
-    [SerializeField] SceneAsset nextScene;
+    //[SerializeField] SceneAsset nextScene;
+    public string nextScene = null;
     bool isTriggered = false;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,8 +20,10 @@ public class TransitionPoint : MonoBehaviour
         
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            Debug.Log(nextScene);
+
             isTriggered = true;
-            GameManager.Instance.ChangeScene(nextScene.name);
+            GameManager.Instance.ChangeScene(nextScene);
         }
     }
 }
